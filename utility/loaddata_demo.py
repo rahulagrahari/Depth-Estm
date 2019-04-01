4c239626cb9e9a12e3e747dae0e9f9ea90443381
Rahul Agrahari <agraharisrm@gmail.com>
2019-04-01 10:35:08 +0100

    inital commit

---
+++ utility/loaddata_demo.py
@@ -0,0 +1,53 @@
+#import pandas as pd
+import numpy as np
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+from PIL import Image
+import random
+from demo_transform import *
+
+
+class depthDataset(Dataset):
+
+    def __init__(self, filename, transform=None):
+        self.frame = filename
+        self.transform = transform
+
+    def __getitem__(self, idx):
+        image = Image.open(self.frame)
+        
+        if self.transform:
+            image = self.transform(image)
+
+        return image
+
+    def __len__(self):
+        return int(1)
+
+
+def readNyu2(filename):
+    __imagenet_stats = {'mean': [0.485, 0.456, 0.406],
+                        'std': [0.229, 0.224, 0.225]}
+
+    image_trans = depthDataset(filename, transform=transforms.Compose([Scale([320, 240]),
+                                                                       ToTensor(),
+                                                                       Normalize(__imagenet_stats['mean'],
+                                                                                 __imagenet_stats['std'])]))
+
+    image = DataLoader(image_trans, batch_size=1, shuffle=False, num_workers=0, pin_memory=False)
+
+    return image
+
+
+def readBulkNyu2(file_List):
+
+    images = []
+    print (file_List)
+    for filename in file_List:
+        print (filename)
+        image = readNyu2(filename)
+        images.append(image)
+
+    return images
+
+
